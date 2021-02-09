5cc70246c80723961804c84459a7ace8c98f58f2
Robzlegz <robzlegz@gmail.com>
2021-02-09 13:23:37 +0200

    setting up firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAHUSYbbTEr6cJEBV79FmoAm4Wg3IpQSIM",
+    authDomain: "mern-whats-app-e31f2.firebaseapp.com",
+    projectId: "mern-whats-app-e31f2",
+    storageBucket: "mern-whats-app-e31f2.appspot.com",
+    messagingSenderId: "642439259990",
+    appId: "1:642439259990:web:7004bdfe88dcceba0798f7"
+};
