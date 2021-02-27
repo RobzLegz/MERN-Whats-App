18426aedf0b7edd60f1cfdefe7bcd238fa661360
Robzlegz <robzlegz@gmail.com>
2021-02-27 17:40:14 +0200

    firebase update

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyAHUSYbbTEr6cJEBV79FmoAm4Wg3IpQSIM",
     authDomain: "mern-whats-app-e31f2.firebaseapp.com",
@@ -6,3 +8,11 @@ const firebaseConfig = {
     messagingSenderId: "642439259990",
     appId: "1:642439259990:web:7004bdfe88dcceba0798f7"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export {auth, provider};
+export default db;
